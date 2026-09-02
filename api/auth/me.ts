55cc0ac16ae787db8b8a46c22015abeb180516cf
Truong Minh Khiem <truongminhkhiemvta@gmail.com>
2026-09-02 22:18:33 +0700

    feat(auth): replace demo login with PostgreSQL accounts

---
+++ api/auth/me.ts
@@ -0,0 +1,2 @@
+import { getUser, send } from "../_lib/auth";
+export default function handler(req:any,res:any) { const user=getUser(req); return user?send(res,200,{user}):send(res,401,{code:"UNAUTHENTICATED"}); }
